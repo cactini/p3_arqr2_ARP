52e0e668534d1061821fc544cde101d0d9ea96eb
cactini <dear.sag@gmail.com>
2026-04-10 14:29:08 +0200

    Create arp frame

--- arp.py
+++ arp.py
@@ -132,7 +132,17 @@ def createARPRequest(ip: int) -> bytes:
     global myMAC, myIP
     frame = bytes()
     logging.debug('Función no implementada')
-    # TODO implementar aqui
+    # DONE implementar aqui
+    '''
+        La trama tendrá la siguiente estructura:
+        |dstMAC|myMAC|ethType|HdType|PtType|HdSize|PtSize|op|myMAC|myIP|trgtMAC|trgtIP|
+        |6B    |6B   |2B     |2B    |2B    |1B    |1B    |2B|6B   |4B  |6B     |4B    |
+    '''
+    requestedIP = ip
+    ethType = 0x0806
+    op = 0x0001
+    emptyMAC = 0x000000000000
+    frame = broadcastAddr + myMAC + ethType + ARPHeader + op + myMAC + myIP + emptyMAC + ip
     # Aquí termina la implementación del alumno
     return frame
 
